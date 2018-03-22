c518488d5d49e8601e9639a7a8ebf4a7f02bbb77
victor <victor.alvarez@live.u-tad.com>
2018-03-22 16:28:12 +0100

    actualizacion

--- PrimerHito/ViewController.swift
+++ PrimerHito/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func accionBotonLogear(){
+        TxtConsola?.text="hola"
+        //txtConsola?.txt=String(format: "Hola %@, con contraseña %@", (txtfusuario?.text)!, (txtfPass?.text)!);
+    }
 
 }
 
